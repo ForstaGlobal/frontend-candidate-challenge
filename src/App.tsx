ce36be75bea9e2e6091a79a1cf18c30a774cf230
jylep <jyl44760@gmail.com>
2022-10-01 16:21:03 +0200

    feat(form): wip - adding form and dummy data, styling up form

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,29 @@ const App: React.FC = () => {
         <figure className='forsta-logo'></figure>
         <h1 className='uppercase'>Task tracker</h1>
       </header>
-      {/* Tasks section */}
+      
+      <section className="tasks">
+        <form className="tasks__form">
+          <input className="tasks__form_field" type="text" name="task" id="task" />
+          <button className="tasks__form_btn" type="submit">Add Task</button>
+        </form>
+
+        <section className="tasks__list">
+          <ul>
+            <li className="task">
+              <div className="task__icon">x</div>
+              <div className="task__description">description</div>
+              <div className="task__icon">t</div>
+            </li>
+
+            <li className="task">
+              <div className="task__icon">x</div>
+              <div className="task__description">description</div>
+              <div className="task__icon">t</div>
+            </li>
+          </ul>
+        </section>
+      </section>
     </div>
   );
 }
