c34af0740c330804e55c14557eb7e415e929ecdf
Vedad K <vkaravdic@haproxy.com>
2022-10-02 17:08:57 +0200

    added tests and fixed testids

--- src/App.tsx
+++ src/App.tsx
@@ -93,10 +93,10 @@ export const App = () => {
   return (
     <div id="theapp" className="todoListApp">
       <div className="sm:flex block">
-        <div className="forsta-logo block sm:inline-block min-w-[240px]" />
+        <div className="forsta-logo block sm:inline-block min-w-[240px]" data-testid="logo" />
         <div className="overflow-auto block sm:inline-block md:w-[100%] md:h-[74px] w-[100%] py-3">
           <label className="inline-flex md:float-right relative items-center cursor-pointer sm:float-right">
-            <input type="checkbox" id="dark-toggle" className="sr-only peer" onChange={toggleDark}/>
+            <input type="checkbox" id="dark-toggle" className="sr-only peer" data-testid="dark-toggle" onChange={toggleDark}/>
             <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-2 peer-focus:ring-zinc-300 dark:peer-focus:ring-zinc-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-zinc-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900">Dark Mode</span>
           </label>
@@ -110,7 +110,7 @@ export const App = () => {
           <button id="saveBtn" className="hidden md:min-w-[auto] min-w-[100%] rounded bg-blue-700 text-white font-bold hover:bg-blue-900 my-3 m-0 block px-5 py-3" onClick={handleSave} >Save</button>
         </div>
         <hr className="my-8 w-[60%] mx-auto dark:border-zinc-600 border-zinc-400" />
-        <span className="text-zinc-900 dark:text-white font-bold text-lg w-[100%] block text-start" onClick={handleEdit}>Current Todos ({todos.length}):</span>
+        <span className="text-zinc-900 dark:text-white font-bold text-lg w-[100%] block text-start" data-testid="todostitle" onClick={handleEdit}>Current Todos ({todos.length}):</span>
         <TodoList todos={todos} handleEdit={handleEdit} handleAdd={handleAdd} handleDel={handleDel} handleDone={handleDone}/>
       </div>
     </div>
--- src/tests/app.spec.tsx
+++ src/tests/app.spec.tsx
@@ -13,12 +13,44 @@ describe("TodoApp", () => {
   it("renders initial items", () => {
     render(<App />);
 
-    expect(screen.getByText("Add Input Element")).toBeDefined();
+    screen.getByTestId("logo");
+    screen.getByTestId("dark-toggle");
+    screen.getByTestId("todostitle");
+    expect(screen.getByText("New Todo:")).toBeDefined();
+    expect(screen.getByText("+")).toBeDefined();
+    expect(screen.getByText("Save")).toBeDefined();
     screen.getByTestId("todo0");
-    expect(screen.getByText("Add Add Button")).toBeDefined();
+    expect(screen.getByText("Fix Dependencies Versions")).toBeDefined();
+    screen.getByTestId("todoelem1");
     screen.getByTestId("todo1");
-    expect(screen.getByText("Get a Coffee")).toBeDefined();
+    screen.getByTestId("edit-1");
+    screen.getByTestId("del-1");
+    expect(screen.getByText("Add Tailwind CSS")).toBeDefined();
+    screen.getByTestId("todoelem2");
     screen.getByTestId("todo2");
+    screen.getByTestId("edit-2");
+    screen.getByTestId("del-2");
+    expect(screen.getByText("Add All Elements")).toBeDefined();
+    screen.getByTestId("todoelem3");
+    screen.getByTestId("todo3");
+    screen.getByTestId("edit-3");
+    screen.getByTestId("del-3");
+    expect(screen.getByText("Create Required Functions")).toBeDefined();
+    screen.getByTestId("todoelem4");
+    screen.getByTestId("todo4");
+    screen.getByTestId("edit-4");
+    screen.getByTestId("del-4");
+    expect(screen.getByText("Create All Tests")).toBeDefined();
+    screen.getByTestId("todoelem5");
+    screen.getByTestId("todo5");
+    screen.getByTestId("edit-5");
+    screen.getByTestId("del-5");
+    expect(screen.getByText("Fix All Types")).toBeDefined();
+    screen.getByTestId("todoelem6");
+    screen.getByTestId("todo6");
+    screen.getByTestId("edit-6");
+    screen.getByTestId("del-6");
+    expect(screen.getByText("Commit All Changes and request Pull")).toBeDefined();
 
   });
 });
