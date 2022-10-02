3f8e60f0430cd888c8c6ec057ea214b9a8579179
Vedad K <vkaravdic@haproxy.com>
2022-10-02 15:04:16 +0200

    className toggle for done items

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -16,7 +16,7 @@ export const TodoList = (props: TodoListProps ) => {
         <li key={i} className="dark:bg-zinc-600 bg-zinc-100 dark:text-white font-bold my-3 mx-auto px-4 py-3 text-start rounded">
         <div className="w-[100%] md:flex md:flex-column">
           <input type="checkbox" className="m-3 p-4" onChange={() => props.handleDone(item)}/>
-          <span data-testid={`todo${i}`} onChange={() => props.handleEdit(this)} key={item.text} className="w-[100%] md:mr-3 dark:bg-zinc-600 rounded dark:text-white block my-3 px-3 py-3">{item.text}</span>
+          <div className="w-[100%] md:mr-3 dark:bg-zinc-600 rounded dark:text-white block my-3 px-3 py-3"><span className={item.done ? "line-through" : ""} data-testid={`todo${i}`} onChange={() => props.handleEdit(this)} key={item.text} >{item.text}</span></div>
           <input type="hidden" className="w-[100%] md:mr-3 dark:bg-zinc-600 rounded dark:text-white border-[1px] block my-3 px-3 py-3 leading-8" placeholder="{item.text}" value={item.text} />
           <button className="md:min-w-[auto] min-w-[100%] rounded bg-green-700 text-white font-bold hover:bg-green-900 my-3 m-0 block px-5 py-3 mr-2" onClick={() => props.handleEdit(item)}>Edit</button>
           <button className="md:min-w-[auto] min-w-[100%] rounded bg-red-700 text-white font-bold hover:bg-red-900 my-3 m-0 block px-5 py-3" onClick={() => props.handleDel(item)}>X</button>
