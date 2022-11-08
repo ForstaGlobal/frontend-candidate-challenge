13620e3be07cb3ffd91fef84b9b1b9ce2a1ad470
Bane <bane.itmedia@gmail.com>
2022-11-08 23:33:29 +0100

    TodoItem component with all functionalities(edit, delete, completed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { TodoList } from "./components/TodoList";
 import "./styles.scss";
+import {TodoForm} from './components/TodoForm';
 
 export default function App() {
 
   return (
-    <div className="todoListApp">
-      <div className="forsta-logo" />
-      <TodoList/>
-    </div>
+      <div className="todoListApp">
+          <div className="forsta-logo"/>
+          <TodoForm/>
+          <TodoList/>
+      </div>
   );
 }
