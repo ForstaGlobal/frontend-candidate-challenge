2eb4b881a8828c4812a66bb1c795edee5ff55a32
Jimpad <hadzicdino99@gmail.com>
2022-12-11 11:36:56 +0100

    Material UI initialization and Dashboard addition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import * as React from 'react';
+import DashboardContent from "../components/dashboard/index";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Todo Application - Forsta</title>
-        <meta name="description" content="Todo application made for the forsta frontend developer challenge." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <footer className={styles.footer}>
-        <a
-          href="https://jimpad.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Jimpad©2022{' '}
-        </a>
-      </footer>
-    </div>
-  )
+export default function Dashboard() {
+  return <DashboardContent />;
 }
