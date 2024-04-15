2a3f52b2ceacc596745f36fe1a11f84bc485bbb5
Sonal <naiksonalp@gmail.com>
2024-04-15 16:40:47 +0200

    Minor fix

--- src/app/components/TodoList/TodoList.tsx
+++ src/app/components/TodoList/TodoList.tsx
@@ -1,32 +1,37 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../../redux/store"; 
+import { RootState } from "../../redux/store";
 import "./TodoList.scss";
 import TodoItem from "../TodoItem/TodoItem";
 import { useMemo } from "react";
 
 function TodoList() {
-  const todos = useSelector(
-    (state: RootState) => state.todo.todos
+  const todos = useSelector((state: RootState) => state.todo.todos);
+  const searchQuery = useSelector((state: RootState) => state.todo.searchQuery);
+  const filterCategory = useSelector(
+    (state: RootState) => state.todo.filterCategory
   );
-  const searchQuery = useSelector( (state: RootState) => state.todo.searchQuery);
-  const filterCategory = useSelector( (state: RootState) => state.todo.filterCategory);
 
   const filteredTodos = useMemo(() => {
-    return todos.filter(todo =>
-      todo.category.toLowerCase().includes(filterCategory.toLowerCase()) &&
-      (todo.task.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      (todo.description?.toLowerCase().includes(searchQuery.toLowerCase())) ||
-      todo.category.toLowerCase().includes(searchQuery.toLowerCase()))
+    return todos.filter(
+      (todo) =>
+        todo.category.toLowerCase().includes(filterCategory.toLowerCase()) &&
+        (todo.task.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          todo.description?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          todo.category.toLowerCase().includes(searchQuery.toLowerCase()))
     );
-  }, [todos, filterCategory, searchQuery]);  
-   
+  }, [todos, filterCategory, searchQuery]);
+
   return (
     <div className="todolist-container">
-      <ul className="todos-container">
-        {filteredTodos.map((todo) => (
-          <TodoItem todo={todo} key={todo.id} />
-        ))}
-      </ul> 
+      {filteredTodos.length === 0 ? (
+        <p>No todo items found</p>
+      ) : (
+        <ul className="todos-container">
+          {filteredTodos.map((todo) => (
+            <TodoItem todo={todo} key={todo.id} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
