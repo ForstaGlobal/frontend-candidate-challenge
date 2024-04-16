16a161a532f80eceee21804d5fe258915c66ca61
Alessio Prestileo <alessio.prestileo@forsta.com>
2024-04-16 09:09:30 -0700

    build: Upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
