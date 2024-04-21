e5a33a9140ebf8dadfe3bb23ad11ad53114e7189
Abdolrahman <rahman.fa.personal@gmail.com>
2024-04-21 11:01:36 +0200

    Added a small animation on the state change of tasks

--- src/containers/MainContent.container.tsx
+++ src/containers/MainContent.container.tsx
@@ -1,6 +1,6 @@
 import Stack from '@mui/material/Stack';
 import { MainContentTopBanner } from 'views/MainContentTopBanner.view';
-import { useContext } from 'react';
+import { useCallback, useContext } from 'react';
 import { AppContext } from 'App';
 import { TasksContainer } from './Tasks.container';
 import { AddTaskContainer } from './AddTask.container';
@@ -8,10 +8,13 @@ import { AddTaskContainer } from './AddTask.container';
 export const MainContentContainer = () => {
   const { categories, tasks, setTasks, filter, setFilter, showDoneTasks } = useContext(AppContext);
 
-  const getTaskColor = (taskCategory: string) => {
-    const matchingCategory = categories.find((category) => category.label === taskCategory);
-    return matchingCategory?.color ?? 'pink';
-  };
+  const getTaskColor = useCallback(
+    (taskCategory: string) => {
+      const matchingCategory = categories.find((category) => category.label === taskCategory);
+      return matchingCategory?.color ?? 'pink';
+    },
+    [categories]
+  );
 
   const getFocusedTask = () => tasks.filter((task) => !task.done)[0];
 
--- src/views/Task.view.tsx
+++ src/views/Task.view.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Circle from '@mui/icons-material/Brightness1';
 import CheckedCircle from '@mui/icons-material/CheckCircleOutline';
@@ -6,6 +6,7 @@ import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UncheckedCircle from '@mui/icons-material/RadioButtonUnchecked';
+import Tooltip from '@mui/material/Tooltip';
 import { TaskType } from 'types';
 import { TestID } from 'testID';
 
@@ -15,33 +16,54 @@ export const Task: React.FC<{
   getTaskColor: (taskCategory: string) => string;
   onDelete: (id: number) => void;
   onDone: (id: number) => void;
-}> = ({ task, onChange, getTaskColor, onDelete, onDone }) => (
-  <Stack direction="row" className="task" alignItems="center">
-    <Circle
-      fontSize="small"
-      className={getTaskColor(task.category)}
-      data-testid={TestID.TaskViewCategory}
-    />
+}> = ({ task, onChange, getTaskColor, onDelete, onDone }) => {
+  const [taskDone, setTaskDone] = useState<boolean>(task.done);
 
-    <TextField
-      placeholder="What is your next task?"
-      variant="standard"
-      value={task.text}
-      onChange={(e) => onChange(task.id, e.target.value)}
-      InputProps={{ disableUnderline: true, className: 'grey' }}
-      sx={{ ml: 2, mr: 2, width: '100%' }}
-      data-testid={TestID.TaskViewTextField}
-    />
+  const onClick = () => {
+    setTaskDone((taskDone) => !taskDone);
 
-    <Typography variant="body1" minWidth="100px" className="grey">
-      {task.time}
-    </Typography>
+    // allows the change in state to be visible to the user before filterings happen.
+    setTimeout(() => {
+      onDone(task.id);
+    }, 500);
+  };
 
-    <DeleteIcon className="light-grey delete-icon" onClick={() => onDelete(task.id)} />
+  return (
+    <Stack direction="row" className="task" alignItems="center">
+      <Circle
+        fontSize="small"
+        className={getTaskColor(task.category)}
+        data-testid={TestID.TaskViewCategory}
+      />
 
-    {!task.done && (
-      <UncheckedCircle className="light-grey done-btn" onClick={() => onDone(task.id)} />
-    )}
-    {task.done && <CheckedCircle className="purple done-btn" onClick={() => onDone(task.id)} />}
-  </Stack>
-);
+      <TextField
+        placeholder="What is your next task?"
+        variant="standard"
+        value={task.text}
+        onChange={(e) => onChange(task.id, e.target.value)}
+        InputProps={{ disableUnderline: true, className: 'grey' }}
+        sx={{ ml: 2, mr: 2, width: '100%' }}
+        data-testid={TestID.TaskViewTextField}
+      />
+
+      <Typography variant="body1" minWidth="100px" className="grey">
+        {task.time}
+      </Typography>
+
+      <Tooltip title="Delete">
+        <DeleteIcon className="light-grey delete-icon" onClick={() => onDelete(task.id)} />
+      </Tooltip>
+
+      {!taskDone && (
+        <Tooltip title="Mark as done">
+          <UncheckedCircle className="light-grey done-btn" onClick={onClick} />
+        </Tooltip>
+      )}
+      {taskDone && (
+        <Tooltip title="Mark as undone">
+          <CheckedCircle className="purple done-btn" onClick={onClick} />
+        </Tooltip>
+      )}
+    </Stack>
+  );
+};
