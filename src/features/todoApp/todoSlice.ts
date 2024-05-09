a1c233453c8e3bd3cf15be993403e65269278a5f
xia0m <alexma325@gmail.com>
2024-05-09 00:33:25 -0700

    added redux slice actions

--- src/features/todoApp/todoSlice.ts
+++ src/features/todoApp/todoSlice.ts
@@ -1,36 +1,71 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
+import { v4 as uuidv4 } from 'uuid';
 
-export interface TodoItem {
+export interface ITodoItem {
     id: string;
+    text: string;
+    done: boolean;
 }
 
 export interface todoState {
-  todos: TodoItem[];
+  todos: ITodoItem[];
   status: 'idle' | 'loading' | 'failed';
 }
 
 const initialState: todoState = {
-  todos: [],
+  todos: [{text: "Buy milk", done: false, id: uuidv4()}],
   status: 'idle',
 };
 
 export const counterSlice = createSlice({
   name: 'todo',
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    addTodo: (state) => {
+    addTodo: (state, action: PayloadAction<string>) => {
+      const newTodo = {
+        id: uuidv4(),
+        text: action.payload,
+        done: false,
+      }
+      return {
+        ...state,
+        todos: [newTodo, ...state.todos],
+      }
     },
-    deleteTodo: (state) => {
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id === action.payload),
+      }
     },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    editTodo: (state, action: PayloadAction<TodoItem>) => {
+    editTodo: (state, action: PayloadAction<ITodoItem>) => {
+      const {id, text} = action.payload;
+      const newTodos = state.todos.map(todo => todo.id === id ?
+        {...todo,
+          text: text,
+        } : todo
+       );
+      return {
+        ...state,
+        todos: newTodos,
+      }
+    },
+    toggleTodo: (state, action: PayloadAction<string>) => {
+      const newTodos = state.todos.map(todo => todo.id === action.payload ?
+        {...todo,
+          done: !todo.done,
+        } : todo
+       );
+      return {
+        ...state,
+        todos: newTodos,
+      }
     },
   },
 });
 
-export const { addTodo, deleteTodo, editTodo } = counterSlice.actions;
+export const { addTodo, deleteTodo, editTodo, toggleTodo } = counterSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
