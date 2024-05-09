0c99afc1ad83b7558383c8f3f9fa09faa56befc5
xia0m <alexma325@gmail.com>
2024-05-09 16:33:18 -0700

    Wrapped add todo input into a form

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import { addTodo } from '../features/todoApp/todoSlice';
 import { useAppDispatch } from '../app/hooks';
 
@@ -7,15 +7,24 @@ export const AddTodo = () => {
 
   const dispatch = useAppDispatch();
 
-  const handleAddTodo = () => {
+  const handleAddTodo = (e: FormEvent) => {
+    e.preventDefault();
     dispatch(addTodo(todoText));
     setTodoText('');
   };
 
   return (
     <div className="addTodo">
-      <input value={todoText} onChange={(e) => setTodoText(e.target.value)} />
-      <button onClick={handleAddTodo}>Add</button>
+      <form onSubmit={handleAddTodo}>
+        <input
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+          placeholder="Enter new todo here"
+          name="todoText"
+          data-testid="new-todo-input"
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 };
